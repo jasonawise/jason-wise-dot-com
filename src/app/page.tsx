9ef8b44a5a6ed811fe7f0c78f6c2a187d737f6fd
Jason A. Wise <jasonandrewwise@gmail.com>
2024-11-22 20:17:23 -0500

    adding h1 styles and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-export default async function Home() {
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Jason A. Wise",
+  description: "...",
+};
+
+export default async function Page() {
   return (
     <div>
       <h1>Welcome!</h1>
